c0a27286b99ad6d0e5d812b7216a285110338906
fjy <fangjin@metamarkets.com>
2013-12-09 19:53:14 -0800

    fix more casing issues

--- src/main/java/io/druid/data/input/impl/TimestampSpec.java
+++ src/main/java/io/druid/data/input/impl/TimestampSpec.java
@@ -25,7 +25,7 @@ public class TimestampSpec
   )
   {
     this.timestampColumn = timestampColumn.toLowerCase();
-    this.timestampFormat = format == null ? defaultFormat : format;
+    this.timestampFormat = format == null ? defaultFormat : format.toLowerCase();
     this.timestampConverter = ParserUtils.createTimestampParser(timestampFormat);
   }
 
