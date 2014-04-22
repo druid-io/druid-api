ec939f05944bed2b62f3b8d158999318a059d93e
Xavier Léauté <xavier@metamarkets.com>
2014-04-21 22:35:56 -0700

    support for null dimValues in groupBy

--- src/main/java/io/druid/data/input/Rows.java
+++ src/main/java/io/druid/data/input/Rows.java
@@ -1,10 +1,14 @@
 package io.druid.data.input;
 
+import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.metamx.common.ISE;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,11 +23,22 @@ public class Rows
     if (row instanceof MapBasedRow) {
       MapBasedRow mapBasedRow = (MapBasedRow) row;
 
-      TreeMap<String, Object> caseInsensitiveMap = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
+      final TreeMap<String, Object> caseInsensitiveMap = Maps.newTreeMap(String.CASE_INSENSITIVE_ORDER);
       caseInsensitiveMap.putAll(mapBasedRow.getEvent());
       return new MapBasedInputRow(
           mapBasedRow.getTimestampFromEpoch(),
-          dimensions,
+          Lists.newArrayList(
+              Iterables.filter(
+                  dimensions, new Predicate<String>()
+                  {
+                    @Override
+                    public boolean apply(@Nullable String input)
+                    {
+                      return caseInsensitiveMap.containsKey(input);
+                    }
+                  }
+              )
+          ),
           caseInsensitiveMap
       );
     }
