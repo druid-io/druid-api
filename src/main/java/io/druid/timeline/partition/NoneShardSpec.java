dbf28e9624cca1ca0dafa82319b49b2577e662dc
nishantmonu51 <nishant.monu51@gmail.com>
2014-07-08 18:02:53 +0530

    Add ShardspecLookup
    Add ShardSpecLookup

--- src/main/java/io/druid/timeline/partition/NoneShardSpec.java
+++ src/main/java/io/druid/timeline/partition/NoneShardSpec.java
@@ -1,6 +1,13 @@
 package io.druid.timeline.partition;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+import com.metamx.common.ISE;
 import io.druid.data.input.InputRow;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
 /**
  */
@@ -22,6 +29,20 @@ public class NoneShardSpec implements ShardSpec
   public int getPartitionNum()
   {
     return 0;
+  }
+
+  @Override
+  public ShardSpecLookup getLookup(final List<ShardSpec> shardSpecs)
+  {
+
+    return new ShardSpecLookup()
+    {
+      @Override
+      public ShardSpec getShardSpec(InputRow row)
+      {
+        return shardSpecs.get(0);
+      }
+    };
   }
 
   @Override
--- src/main/java/io/druid/timeline/partition/ShardSpec.java
+++ src/main/java/io/druid/timeline/partition/ShardSpec.java
@@ -2,6 +2,9 @@ package io.druid.timeline.partition;
 
 import io.druid.data.input.InputRow;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * A Marker interface that exists to combine ShardSpec objects together for Jackson
  */
@@ -10,4 +13,5 @@ public interface ShardSpec
   public <T> PartitionChunk<T> createChunk(T obj);
   public boolean isInChunk(InputRow inputRow);
   public int getPartitionNum();
+  public ShardSpecLookup getLookup(List<ShardSpec> shardSpecs);
 }
