18071ecdb9fb54abe43b5f4fbed7019e9260a819
nishantmonu51 <nishant.monu51@gmail.com>
2014-12-16 22:15:06 +0530

    move DataSegmentTest to druid-api

--- src/main/java/io/druid/timeline/partition/ShardSpec.java
+++ src/main/java/io/druid/timeline/partition/ShardSpec.java
@@ -1,17 +1,25 @@
 package io.druid.timeline.partition;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.druid.data.input.InputRow;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * A Marker interface that exists to combine ShardSpec objects together for Jackson
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+                  @JsonSubTypes.Type(name = "none", value = NoneShardSpec.class),
+              })
 public interface ShardSpec
 {
   public <T> PartitionChunk<T> createChunk(T obj);
+
   public boolean isInChunk(long timestamp, InputRow inputRow);
+
   public int getPartitionNum();
+
   public ShardSpecLookup getLookup(List<ShardSpec> shardSpecs);
 }
