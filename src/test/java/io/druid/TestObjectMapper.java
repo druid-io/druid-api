104f1819741b92041e3ede7fe10114121bb45cf6
nishantmonu51 <nishant.monu51@gmail.com>
2014-12-17 00:02:23 +0530

    Revert "move DataSegmentTest to druid-api"
    This reverts commit 18071ecdb9fb54abe43b5f4fbed7019e9260a819.

--- src/test/java/io/druid/TestObjectMapper.java
+++ src/test/java/io/druid/TestObjectMapper.java
@@ -1,18 +1,9 @@
 package io.druid;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.joda.time.Interval;
-
-import java.io.IOException;
 
 /**
  */
@@ -26,26 +17,5 @@ public class TestObjectMapper extends ObjectMapper
     configure(MapperFeature.AUTO_DETECT_IS_GETTERS, false);
     configure(MapperFeature.AUTO_DETECT_SETTERS, false);
     configure(SerializationFeature.INDENT_OUTPUT, false);
-    registerModule(new TestModule());
-  }
-
-  public static class TestModule extends SimpleModule
-  {
-    TestModule()
-    {
-      addSerializer(Interval.class, ToStringSerializer.instance);
-      addDeserializer(
-          Interval.class, new StdDeserializer<Interval>(Interval.class)
-          {
-            @Override
-            public Interval deserialize(
-                JsonParser jsonParser, DeserializationContext deserializationContext
-            ) throws IOException, JsonProcessingException
-            {
-              return new Interval(jsonParser.getText());
-            }
-          }
-      );
-    }
   }
 }
