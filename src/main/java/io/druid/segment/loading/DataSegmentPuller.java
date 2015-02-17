2eb740e113aaa4b2cfc25508bc5bcb7cf79d38fa
Charles Allen <charles@allen-net.com>
2015-02-17 11:44:57 -0800

    Changed data pulling for the DataSegmentPuller and LoadSpec * Also moved most functionality of CompresisonUtils away from here (now in java-util) * Added URIDataPuller interface

--- src/main/java/io/druid/segment/loading/DataSegmentPuller.java
+++ src/main/java/io/druid/segment/loading/DataSegmentPuller.java
@@ -5,6 +5,7 @@ import io.druid.timeline.DataSegment;
 import java.io.File;
 
 /**
+ * A DataSegmentPuller is responsible for pulling data for a particular segment into a particular directory
  */
 public interface DataSegmentPuller
 {
@@ -12,7 +13,8 @@ public interface DataSegmentPuller
    * Pull down segment files for the given DataSegment and put them in the given directory.
    *
    * @param segment The segment to pull down files for
-   * @param dir The directory to store the files in
+   * @param dir     The directory to store the files in
+   *
    * @throws SegmentLoadingException if there are any errors
    */
   public void getSegmentFiles(DataSegment segment, File dir) throws SegmentLoadingException;
